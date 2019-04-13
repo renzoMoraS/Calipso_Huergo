16ef1fa7f463ea5fbc5095779dfd8e5e84e345d7
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-04-13 19:25:08 -0300

    Its all a quilombo

--- GMap/js/script.js
+++ GMap/js/script.js
@@ -1,15 +1,39 @@
 function inicializar(){
-    var map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    var map = L.map('map').setView([-17.0568696, -64.9912286], 3);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+        mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; ' + mapLink + ' Contributors',maxZoom: 18,}).addTo(map);
+    
+        fs.readFile('../Mapa/buyerData.json', function(err, data) {
+            var marks = JSON.parse(data);
+            
+            var map = L.map('map').setView([-17.0568696, -64.9912286], 3);
+            mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+            L.tileLayer(
+                'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; ' + mapLink + ' Contributors',
+                maxZoom: 18,
+                }).addTo(map);
+    
+            for (var i = 0; i < marks.length; i++) {
+                marker = new L.marker([marks[i].lat,marks[i].long]).bindPopup(marks[i].name).addTo(map);
+            }
+        });
 }
 
 window.onload = inicializar;
 
+/* 
+
+
+        $.getJSON( "../../Mapa/buyerData.json", function( data ) {
+        var marks = JSON.parse(data);
+    
+        for (var i = 0; i < marks.length; i++) {
+            marker = new L.marker([marks[i].lat,marks[i].long]).bindPopup(marks[i].name).addTo(map);
+        }
+        });
+
+*/
 
--- Mapa/map.js
+++ Mapa/map.js
@@ -1,27 +1,31 @@
 const fs = require('fs');
 var request = require("request")
 
-var url = "https://api.mercadolibre.com/orders/search/archived?seller=425828201&access_token="
+var url = "https://api.mercadolibre.com/orders/search?seller=425828201&access_token="
 
 fs.readFile('../ServerFede/token.json', function(err, data) {
-  var my_token = JSON.parse(data);
-  url = url + my_token["access_token"];
+    var my_token = JSON.parse(data);
+    url = url + my_token["access_token"];
 
-  request({url: url,json: true}, function (error, response, body) {
+    request({url: url,json: true}, function (error, response, bodi) {
+        var name;
+        var lat;
+        var long;
+        var latlong;
 		if (!error && response.statusCode === 200) {
-			for (var i = 0; i <= body.available_filters[6].values.length; i++) {
-				if (body.available_filters[6].values[i] != undefined) {
-				console.log(body.available_filters[6].values[i].id);
-				}
-			}
-    }else{
-      console.log(body);
-    }
-  });
+            if (Object.keys(bodi.results).length != undefined){
+                for (var i = 0; i < Object.keys(bodi.results).length; i++) {
+                    name = JSON.stringify(bodi.results[i].buyer.nickname);
+                    lat = JSON.stringify(bodi.results[i].shipping.receiver_address.latitude);
+                    long = JSON.stringify(bodi.results[i].shipping.receiver_address.longitude);
+                    latlong = '{ "name" : ' + name + ', "lat" : "' + lat + '", "long" : "'+ long + '"}' + '\n';
+                    fs.appendFile('./buyerData.json',latlong, function (err) {if (err) throw err;});     
+                }
+            }else{
+                console.log('error indefinido');    
+            }    
+        }else{
+            console.log('error');
+        }
+    });
 });
-//shipping.service_id
-
-
-//  var direccion = comprador.shipping.receiver_address.country.name+ ", " +comprador.shipping.receiver_address.state.name + ", " +comprador.shipping.receiver_address.city.name+ ", " +comprador.shipping.receiver_address.address_line+"\n"+"\n";
-//  fs.appendFile('./LugaresDeMisCompradores.txt',direccion, function (err) {if (err) throw err;});
-
