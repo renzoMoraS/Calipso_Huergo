f41a6fbaa60f2f470f60748beec28f24c4518359
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-04-13 15:19:49 +0000

    order id list

--- GMap/js/script.js
+++ GMap/js/script.js
@@ -1,40 +1,15 @@
- // JavaScript source code
-function inicializar() {
-    //Opciones del mapa
-    var OpcionesMapa = {
-        center: new google.maps.LatLng(-24.5000000, -69.2500000),
-        mapTypeId: google.maps.MapTypeId.SATELLITE, //ROADMAP  SATELLITE HYBRID TERRAIN
-        mapMaker: true,
-        zoom: 3
-    };
- 
-    var miMapa;
-    //constructor
-    miMapa = new google.maps.Map(document.getElementById('mapa'), OpcionesMapa);
+function inicializar(){
+    var map = L.map('map').setView([51.505, -0.09], 13);
 
-    var geocoder = new google.maps.Geocoder();
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-    var address = "Aristobulo del Valle 1444, Barracas, Ciudad Autonoma de Buenos Aires, Buenos Aires, Argentina";
-    if(address!=''){
-        // Llamamos a la función geodecode pasandole la dirección que hemos introducido en la caja de texto.
-        geocoder.geocode({ 'address': address}, function(results, status){
-            if (status == 'OK'){
-                // Posicionamos el marcador en las coordenadas obtenidas
-                miMapa.marker = new google.maps.Marker({position: {lat: 0, lng: 0}});
-                miMapa.marker.setPosition(results[0].geometry.location);
-                miMapa.marker.setMap(miMapa.map);
-            }else{
-                console.log(status);
-            }
-        });
-    }
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
 }
- 
-function CargaScript() {
-    var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=inicializar';
-    document.body.appendChild(script);                 
-}
- 
-window.onload = CargaScript;
+
+window.onload = inicializar;
+
 
--- Mapa/map.js
+++ Mapa/map.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 var request = require("request")
 
-var url = "https://api.mercadolibre.com/orders/1986165555?access_token="
+var url = "https://api.mercadolibre.com/orders/search/archived?seller=425828201&access_token="
 
 fs.readFile('../ServerFede/token.json', function(err, data) {
   var my_token = JSON.parse(data);
@@ -9,19 +9,19 @@ fs.readFile('../ServerFede/token.json', function(err, data) {
 
   request({url: url,json: true}, function (error, response, body) {
 		if (!error && response.statusCode === 200) {
-      var mybuyer = JSON.stringify(body);
-      fs.writeFile('./buyerData.json',mybuyer, function (err) {if (err) throw err;});
+			for (var i = 0; i <= body.available_filters[6].values.length; i++) {
+				if (body.available_filters[6].values[i] != undefined) {
+				console.log(body.available_filters[6].values[i].id);
+				}
+			}
     }else{
       console.log(body);
     }
   });
 });
+//shipping.service_id
 
-fs.readFile('./buyerData.json', function(err, data) {
 
-  var comprador = JSON.parse(data);
-  var direccion = comprador.shipping.receiver_address.country.name+ ", " +comprador.shipping.receiver_address.state.name + ", " +comprador.shipping.receiver_address.city.name+ ", " +comprador.shipping.receiver_address.address_line+"\n"+"\n";
-  fs.appendFile('./LugaresDeMisCompradores.txt',direccion, function (err) {if (err) throw err;});
-
-});
+//  var direccion = comprador.shipping.receiver_address.country.name+ ", " +comprador.shipping.receiver_address.state.name + ", " +comprador.shipping.receiver_address.city.name+ ", " +comprador.shipping.receiver_address.address_line+"\n"+"\n";
+//  fs.appendFile('./LugaresDeMisCompradores.txt',direccion, function (err) {if (err) throw err;});
 
