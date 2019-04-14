f246b92cf52fef62cf479d28582c979433ac3d5d
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-04-14 15:14:47 -0300

    mapinho

--- GMap/server.js
+++ GMap/server.js
@@ -1,16 +1,10 @@
 var http = require('http');  
 var url = require('url');  
 var fs = require('fs');  
+
 var server = http.createServer(function(request, response) {  
     var path = url.parse(request.url).pathname;  
     switch (path) {  
-        case '/':  
-            response.writeHead(200, {  
-                'Content-Type': 'text/plain'  
-            });  
-            response.write("Mapa");  
-            response.end();  
-            break;  
         case '/index.html':  
             fs.readFile(__dirname + path, function(error, data) {  
                 if (error) {  
