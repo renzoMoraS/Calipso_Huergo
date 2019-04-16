52d6ef7da90979a38d53d3e568802df3f477bc0b
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-04-16 00:09:50 -0300

    simplificandouuu

--- ServerFede/datos_acceso.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    client_id: app_id = '4069477448135367',
-    client_secret: secret_key = 'eqaPB8Ot1neu4JVVGyqDu5tPorwvmlh2',
-    redirect_uri: redirect_uri = 'http://localhost:8081/logued_in',
-    app_name: app_name = 'HuergoCalipso'
-}
