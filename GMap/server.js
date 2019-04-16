46d7e7483760349b435a3fd8db3eb1f7494f70f6
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-04-16 00:04:43 -0300

    simplificando

--- GMap/server.js
+++ GMap/server.js
@@ -1,38 +1,83 @@
-var http = require('http');  
-var url = require('url');  
-var fs = require('fs');  
+var cookieParser = require('cookie-parser') 
+var datos_acceso = require('./js/datos_acceso')
+var http = require('http');
+var request = require('request')
+var fs = require('fs');
+var express = require('express');
 
-var server = http.createServer(function(request, response) {  
-    var path = url.parse(request.url).pathname;  
-    switch (path) {  
-        case '/index.html':  
-            fs.readFile(__dirname + path, function(error, data) {  
-                if (error) {  
-                    response.writeHead(404);  
-                    response.write(error);  
-                    response.end();  
-                } else {  
-                    response.writeHead(200, {  
-                        'Content-Type': 'text/html'  
-                    });  
-                    response.write(data);  
-                    response.end();  
-                }  
-            });  
-            break;
-        case '/buyerData.json':
+var app = express(); 
+app.use(cookieParser())
+
+var valores = {"grant_type":"authorization_code",
+    "client_id": datos_acceso.client_id,
+    "client_secret": datos_acceso.client_secret,
+    "redirect_uri": datos_acceso.redirect_uri,
+    "code": ""}; 
+
+var options = {
+   url:'https://api.mercadolibre.com/oauth/token', 
+   form: valores, 
+   method: "POST",
+   headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/json' 
+   }
+};
+
+var preguntarAML = function(error,response,body) {
+  fs.writeFile('./json/token.json',body, function (err) {if (err) throw err;});
+  console.log(body); 
+  console.log(error); 
+}
+
+
+app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" ); 
+    
+    var uuid_sesion = (Math.random() * (1 - 200) + 200).toString()
+    res.cookie('session_id', uuid_sesion, { maxAge: 900000, httpOnly: true });
+    
+    console.log('uuid_sesion: '+uuid_sesion)
+})
+
+
+app.get('/logued_in', function (req, res) {
+    session_cookie = req.cookies['session_id'] 
+    console.log(session_cookie); 
+    
+    if (req.query.error != null){ 
+        console.log('error '+req.query.error)
+    };
+
+    
+    valores.code = req.query.code 
+    console.log('codigo acceso:\n') 
+    console.log(valores.code)
+    console.log('pido token\n')
+
+    var req = request.post(options, preguntarAML);
+    res.send('<input type="button" onclick="location.href=\'http://localhost:8081/map.html\'" value="Mapa" />') 
+})
+
+app.get('/buyerData.json',function(req,response){
             response.writeHead(200, {  
                 'Content-Type': 'application/json'  
             });  
 
-            fs.readFile('./buyerData.json', function (err, data) {
+            fs.readFile('json/buyerData.json', function (err, data) {
                 if (err) throw err;
                 var bd = JSON.parse(data);  
                 response.end(JSON.stringify(bd));
-              });
-                
-            break;
-    }  
-});  
+            });
+})
 
-server.listen(8082); 
+app.get('/map.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "map.html" ); 
+    
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Server de MercadoLibre4AO, en http://%s:%s", host, port)
+})
