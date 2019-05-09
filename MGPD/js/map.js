b11ff2e82d50063881f070af9bde9800b01f5ca6
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-05-09 00:34:04 +0000

    Arreglando Cluster

--- MGPD/js/map.js
+++ MGPD/js/map.js
@@ -15,7 +15,6 @@ exports.buscarCompradores=function(){
             var latlong;
             var cont = 0;
             var same = false;
-            var dif = false;
 		    if (!error && response.statusCode === 200) {
                 fs.writeFile('./json/buyerData.json','{"Address":[]}', function (errore) {if (errore) throw errore;});
                 
@@ -23,28 +22,17 @@ exports.buscarCompradores=function(){
                     var bjason = JSON.parse(dataso);
                     if (Object.keys(bodi.results).length != undefined && Object.keys(bodi.results).length != 0 ){
                         for (var i = 0; i < Object.keys(bodi.results).length; i++) {
-                            console.log(bodi.results[i].shipping);
-                            dif = false;
-                                if (bodi.results[i].shipping.receiver_address != undefined && bodi.results[i].shipping.receiver_address.id != null){
-                                    for (var j = 0; j < Object.keys(bodi.results).length; j++) {
-                                        if (bodi.results[i].shipping.receiver_address.id == bodi.results[j].shipping.receiver_address.id) {
-                                            if (bodi.results[i].shipping.receiver_address.id == bodi.results[j].shipping.receiver_address.id) {
-                                                var dif = false;
-                                            }
-                                        }
-                                    }
-                                    if (dif == true) {
-                                        lat = JSON.stringify(bodi.results[i].shipping.receiver_address.latitude);
-                                        long = JSON.stringify(bodi.results[i].shipping.receiver_address.longitude);
-                                        latlong = {"lat" : lat , "long" : long };
+                            console.log(bodi.results[i].shipping.receiver_address);
+                
+                            if (bodi.results[i].shipping.receiver_address != undefined && bodi.results[i].shipping.receiver_address.id != null){
+                                lat = JSON.stringify(bodi.results[i].shipping.receiver_address.latitude);
+                                long = JSON.stringify(bodi.results[i].shipping.receiver_address.longitude);
+                                latlong = {"lat" : lat , "long" : long };
                             
-                                        bjason.Address[cont] = latlong;
-                                        fs.writeFile('./json/buyerData.json',JSON.stringify(bjason), function (errore) {if (errore) throw errore;});
-                                        cont++;
-                                    }
-                                        
-                                    
-                                }else{console.log('No se puede saber la ubicacion del comprador');}
+                                bjason.Address[cont] = latlong;
+                                fs.writeFile('./json/buyerData.json',JSON.stringify(bjason), function (errore) {if (errore) throw errore;});
+                                cont++;
+                            }else{console.log('No se puede saber la ubicacion del comprador');}
              
                         }
                     }
